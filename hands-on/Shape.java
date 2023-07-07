7a4b7e9ed909c1935ddf96cd53398d8154fc1f37
sami210801 <138561742+sami210801@users.noreply.github.com>
2023-07-07 13:59:14 +0530

    Add files via upload

--- hands-on/Shape.java
+++ hands-on/Shape.java
@@ -1,31 +1,59 @@
 package oops;
-//example of single inheritance
- class Shape 
-{
-	 //fields and methods 
-	public void calculateArea()
-	{
-		System.out.println("calculating the area of the shape");
 
-	}
+ abstract class Shape
+ {
+public abstract void calculateArea ();
+public abstract void calculatePerimeter();
 
 }
- //inherits from shape
- class Rectangle extends Shape
- {
-	 //fields and methods
+    class Rectangle extends Shape 
+{
+	public void calculatePerimeter()
+	{
+		System.out.println("perimeter of rectangle:p=2+(l+w)");
+	}
+	public void calculateArea()
+	{
+		System.out.println("area of rectangle:a=(wl)");
+	}
+}
+    class Circle extends Shape 
+{
+	 public void calculatePerimeter()
+	{
+		System.out.println("perimeter of circle:C=2πr");
+	}
 	 public void calculateArea()
-	 {
-	 System.out.println("calculating area of rectangle");
- }
- }
- class shapeMain
- {
-	 //creating object of subclass
-	 public static void main(String[] args)
-	 {
-		 Rectangle r= new Rectangle();
-		 r.calculateArea();
-	 }
- }
-
+		{
+			System.out.println("area of circle:A=πr2");
+		}
+	}
+    class Triangle extends Shape 
+{
+	 public void calculatePerimeter()
+	{
+		System.out.println("perimeter of triangle:p=a+b+c");
+	}
+	 public void calculateArea()
+			{
+				System.out.println("area of triangle:A=hbb\r\n");
+			}
+}
+class ShapeMain
+{
+	public static void main(String [] args)
+	{
+	Rectangle r=new Rectangle();
+	r.calculateArea();
+	r.calculatePerimeter();
+	
+	Circle c=new Circle();
+	c.calculateArea();
+	c.calculatePerimeter();
+	
+	Triangle t=new Triangle();
+	t.calculateArea();
+	t.calculatePerimeter();
+	}
+}
+			
