15a00a28014b783ec65b0d58f5d3da2894c01747
sami210801 <138561742+sami210801@users.noreply.github.com>
2023-07-11 12:58:39 +0530

    Add files via upload

---
+++ practical/IfElse.java
@@ -0,0 +1,24 @@
+package oops;
+import java.util.*;
+public class IfElse {
+	public static void main(String[] args)
+	{
+	//Scanner class to take input from the user
+	Scanner= new Scanner(System.in);
+	System.out.println("Enter a value:");
+	
+	//integer input from the user 
+	int a=s.nextInt();
+	
+	//condition
+	if(a>=18)
+	{
+		System.out.println("Eligible for voting");
+
+	}
+	else 
+	{
+		System.out.println("Not Eligible for voting");
+	}
+	}
+}
