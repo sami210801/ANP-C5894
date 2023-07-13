a50aab4c1cf80f4c76925a5338fe5d2766fac650
sami210801 <138561742+sami210801@users.noreply.github.com>
2023-07-13 13:24:33 +0530

    Add files via upload

---
+++ practical/Forloop.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class Forloop {
+	//Simple For Loop example
+
+		public static void main(String[] args)
+		{
+			
+			for(int i=0;i<=10;i++)
+			{
+				System.out.println("Values of i is :"+ i);
+			}
+
+		}
+	}
+	
