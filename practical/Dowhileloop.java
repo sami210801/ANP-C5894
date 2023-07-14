6eedcd17408c80206a47bdf0ee3b9b3a66992578
sami210801 <138561742+sami210801@users.noreply.github.com>
2023-07-14 14:11:55 +0530

    Add files via upload

---
+++ practical/Dowhileloop.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class Dowhileloop {
+public static void main(String[]args)
+{
+	int i=1;
+	do
+			{
+		System.out.println("hello world");
+		i++;
+				
+			}
+	while(i<6);
+}
+	
+}
