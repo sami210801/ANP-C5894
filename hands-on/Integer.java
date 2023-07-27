706c167a56e97a4d482534272decb913463bbf1b
sami210801 <138561742+sami210801@users.noreply.github.com>
2023-07-27 10:42:33 +0530

    Add files via upload

--- hands-on/Integer.java
+++ hands-on/Integer.java
@@ -1,31 +1,22 @@
-package oops;
-import java.util.*;
+package operator;
+/* Write a Java program that takes an integer input from
+the user and checks if it is even or odd using bitwise operators. 
+Print "Even" if the number is even, and "Odd" if the number is odd.
+*/
+import java.util.Scanner;
 public class Integer {
-	 public static void main(String[]args)
-	 {
-		 Scanner n= new Scanner(System.in);
-			System.out.println("Enter a value:");
-			int c=n.nextInt();
-			if(c % 2==0)
-			{
-				System.out.println("divisible by 2");
-			}
-			else if (c % 3==0)
-			{
-				System.out.println("divisible by 2");
-			}
-				else if (c % 2==0 & 3==0)
-				{
-					System.out.println("divisible by 2");
-				
-					
-				}
-				else 
-				{
-					System.out.println("not divisible by 2");
-				}
-		}
-	 }
 
+	public static void main(String[] args) {
+	//creating object for scanner class	
+Scanner sc = new Scanner(System.in);
+//input the first integer
+System.out.println("Enter the number");
+int num =sc.nextInt();
+// n^1 is n+1, then even, else odd
+if ((num ^ 1) == (num + 1))
+	System.out.println("it is even number");
+else
+	System.out.println("it is odd number");
+	}
 
-
+}
