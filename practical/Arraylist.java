79918c3dcfda012d1f46f78f4d57f820d308c543
Sami Shaikh ismail <138561742+sami210801@users.noreply.github.com>
2023-08-10 13:07:39 +0530

    Add files via upload

---
+++ practical/Arraylist.java
@@ -0,0 +1,25 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		
+	       // Create a linked list of strings
+	        LinkedList<String> ls = new LinkedList<>();
+
+	        // Adding elements to the linked list
+	        ls.add("Apple");
+	        ls.add("Banana");
+	        ls.add("Orange");
+	        ls.add("Mango");
+            ls.add("Grapes");
+	        // Accessing elements in the linked list
+	        System.out.println("Linked List: " + ls);
+ls.set(1, "pineapple");
+System.out.println("Linked List: " + ls);
+
+}
+
+}
