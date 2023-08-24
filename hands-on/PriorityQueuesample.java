616b33f03ab5c157ed6d1b80d5dfd990986e8fed
Sami Shaikh ismail <138561742+sami210801@users.noreply.github.com>
2023-08-24 11:28:06 +0530

    Add files via upload

---
+++ hands-on/PriorityQueuesample.java
@@ -0,0 +1,25 @@
+package collection;
+import java.util.*;
+import java.util.PriorityQueue;
+public class PriorityQueuesample {
+
+	public static void main(String[] args) {
+		
+	
+
+			
+				PriorityQueue<Integer>pq= new PriorityQueue<Integer>();
+				pq.add(21);
+				pq.add(16);
+				pq.add(18);
+				pq.add(25);
+				System.out.println("Total elements are:"+pq);
+				System.out.println("Top elements are:"+pq.peek());
+				System.out.println("Remove element is :"+pq.poll());
+				System.out.println("Total elements are:"+pq);
+			}
+
+		}
+
+
+	
