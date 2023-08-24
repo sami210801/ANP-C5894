7bd641c0bce45ca529823c64b407ccd13a37c20b
Sami Shaikh ismail <138561742+sami210801@users.noreply.github.com>
2023-08-24 11:31:06 +0530

    Add files via upload

---
+++ practical/TreeSetsample.java
@@ -0,0 +1,16 @@
+package collection;
+import java.util.*;
+import java.util.Set;
+public class TreeSetsample {
+
+	public static void main(String[] args) {
+	Set<String> s= new TreeSet<String>();
+	s.add("A");
+	s.add("B");
+	s.add("C");
+	s.add("D");
+	System.out.println("Elements are:"+s);
+	
+	}
+
+}
